3eec021010ac618040f5228bd0596f7edbc4c69d
iamxettry <rajuchhetry11@gmail.com>
2024-09-01 22:59:01 +0545

    feat : Blog datas added

---
+++ src/constants/BlogData.js
@@ -0,0 +1,12 @@
+export const BlogData = [
+    {
+      title: "The Art of Perfectly Grilled Steak",
+      image: "https://images.unsplash.com/photo-1654605143281-69539497b345?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fEdyaWxsZWQlMjBjaGlja2VufGVufDB8fDB8fHww",
+      desc: "Master the art of grilling the perfect steak with these simple yet effective tips. Learn how to select the right cut, season it to perfection, and achieve that ideal charred crust while keeping the inside tender and juicy."
+    },
+    {
+      title: "Exploring the World of Artisan Breads",
+      image: "https://plus.unsplash.com/premium_photo-1722596601607-5079628d2fb3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8QXJ0aXNhbiUyMEJyZWFkc3xlbnwwfHwwfHx8MA%3D%3D",
+      desc: "Dive into the rich history and techniques of artisan bread-making. From sourdough to baguettes, discover the unique flavors and textures that make these breads stand out, and learn how you can bake them at home."
+    }
+  ];
