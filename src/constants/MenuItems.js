8cd69151536d4d96da310aa0ca7e750937d1e80c
iamxettry <rajuchhetry11@gmail.com>
2024-09-01 23:23:51 +0545

    feat : menu data added

---
+++ src/constants/MenuItems.js
@@ -0,0 +1,77 @@
+export const TrendingRecipes = [
+  {
+    name: "Onion Rings",
+    image:
+      "https://plus.unsplash.com/premium_photo-1723795263816-fc4e2752b8c6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Zm9vZCUyMGNsaWNraW5nJTIwYnklMjBwaG9uZXxlbnwwfHwwfHx8MA%3D%3D",
+    calorie: 122,
+  },
+  {
+    name: "Caesar Salad",
+    image:
+      "https://images.unsplash.com/photo-1551183053-bf91a1d81141?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Y2Flc2FyJTIwc2FsYWR8ZW58MHx8MHx8fDA%3D",
+    calorie: 180,
+  },
+  {
+    name: "Grilled Chicken",
+    image:
+      "https://images.unsplash.com/photo-1534797258760-1bd2cc95a5bd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8R3JpbGxlZCUyMGNoaWNrZW58ZW58MHx8MHx8fDA%3D",
+    calorie: 250,
+  },
+  {
+    name: "Spaghetti Carbonara",
+    image:
+      "https://plus.unsplash.com/premium_photo-1674511582428-58ce834ce172?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8U3BhZ2hldHRpJTIwQ2FyYm9uYXJhfGVufDB8fDB8fHww",
+    calorie: 400,
+  },
+  {
+    name: "Cheeseburger",
+    image:
+      "https://images.unsplash.com/photo-1568901346375-23c9450c58cd?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGNoZWVzZWJ1cmdlcnxlbnwwfHwwfHx8MA%3D%3D",
+    calorie: 350,
+  },
+  {
+    name: "Chocolate Cake",
+    image:
+      "https://images.unsplash.com/photo-1588195538326-c5b1e9f80a1b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Q2hvY29sYXRlJTIwQ2FrZXxlbnwwfHwwfHx8MA%3D%3D",
+    calorie: 450,
+  },
+];
+
+export const ExploreRecipes = [
+  {
+    name: "Avocado Toast",
+    image:
+      "https://images.unsplash.com/photo-1541519227354-08fa5d50c44d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8QXZvY2FkbyUyMFRvYXN0fGVufDB8fDB8fHww",
+    calorie: 250,
+  },
+  {
+    name: "Pancakes with Berries",
+    image:
+      "https://plus.unsplash.com/premium_photo-1699128808395-59246860f132?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8UGFuY2FrZXMlMjB3aXRoJTIwQmVycmllc3xlbnwwfHwwfHx8MA%3D%3D",
+    calorie: 320,
+  },
+  {
+    name: "Sushi Platter",
+    image:
+      "https://images.unsplash.com/photo-1546069901-eacef0df6022?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8U3VzaGklMjBQbGF0dGVyfGVufDB8fDB8fHww",
+    calorie: 180,
+  },
+  {
+    name: "Vegetarian Pizza",
+    image:
+      "https://plus.unsplash.com/premium_photo-1723478407550-a4a3dacd77f4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8VmVnZXRhcmlhbiUyMFBpenphfGVufDB8fDB8fHww",
+    calorie: 270,
+  },
+  {
+    name: "Greek Salad",
+    image:
+      "https://images.unsplash.com/photo-1657014193301-54e4442fa2fe?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8R3JlZWslMjBTYWxhZHxlbnwwfHwwfHx8MA%3D%3D",
+    calorie: 160,
+  },
+  {
+    name: "Berry Smoothie Bowl",
+    image:
+      "https://images.unsplash.com/photo-1638813133319-897c2e3eb13b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fEJlcnJ5JTIwU21vb3RoaWUlMjBCb3dsfGVufDB8fDB8fHww",
+    calorie: 200,
+  },
+];
