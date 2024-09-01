6f95977deb6dc47b69db5f841500609cc77d8f0a
iamxettry <rajuchhetry11@gmail.com>
2024-09-01 20:54:48 +0545

    feat : Share recipe component created

---
+++ src/Components/pages/Home/ShareRecipeBanner.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ShareRecipe } from '../../../assets';
+import Button from '../../common/Button';
+
+const ShareRecipeBanner = () => {
+  return (
+    <div className="flex flex-col md:flex-row items-center rounded-md  my-24 md:my-44">
+        
+      {/* Image Section */}
+      <div className="md:flex-1">
+        <img 
+          src={ShareRecipe}
+          alt="Share your recipes" 
+          className=" h-96 w-96 object-cover"
+        />
+      </div>
+      
+      {/* Text Section */}
+      <div className="  md:flex-1 mt-6 md:mt-0 md:ml-8 text-center ">
+        <h2 className="text-4xl lg:text-5xl font-bold text-gray-800">
+          Share Your <span className="text-red-500">Recipes</span>
+        </h2>
+        <p className="mt-4 text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iste libero repellendus, adipisci earum nihil. Sequi molestiae dicta vitae laborum!
+        </p>
+        <Button className="mt-6 py-3">
+          <a href="/create-new-recipe">Create New Recipe</a>
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default ShareRecipeBanner;
