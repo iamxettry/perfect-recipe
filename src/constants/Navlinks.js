f1140393f51d57d0cac37e04f9f8f1d163ef1385
iamxettry <rajuchhetry11@gmail.com>
2024-09-01 16:07:50 +0545

    feat : navlinks added

---
+++ src/constants/Navlinks.js
@@ -0,0 +1,24 @@
+export const Navlinks = [
+    {
+      name: "Home",
+      url: "/",
+    },
+    {
+      name: "Recipe",
+      url: "/recipe",
+    },
+    {
+      name: "Add Recipe",
+      url: "/add-recipe",
+    },
+    {
+      name: "Blog",
+      url: "/blog",
+    },
+    {
+      name: "About Us",
+      url: "/about",
+    },
+    
+  ];
+  
