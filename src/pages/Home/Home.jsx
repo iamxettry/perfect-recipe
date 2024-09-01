5ad351b187d0be0e53f74e1300190d36295f54c5
iamxettry <rajuchhetry11@gmail.com>
2024-09-01 22:30:52 +0545

    feat : Trending recipes added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import Layout from '../../Components/layout/Layout'
+import React from "react";
+import WaveSVG from "../../Components/common/WaveSVG";
+import HeroSection from "../../Components/pages/Home/HeroSection";
+import ShareRecipeBanner from "../../Components/pages/Home/ShareRecipeBanner";
+import Container from "../../Components/layout/Container";
+import MenuItemCard from "../../Components/layout/MenuItemCard";
+import { TrendingRecipes } from "../../constants/MenuItems";
 
 const Home = () => {
-  return (
-    <Layout>Home</Layout>
-  )
-}
 
-export default Home
+  return (
+    <>
+      {/* background svg wave */}
+      <WaveSVG />
+
+      {/* Hero section */}
+      <HeroSection />
+
+      {/* Share Recipe Banner  */}
+      <ShareRecipeBanner />
+
+      {/* Trending Recipe */}
+      <Container title={"Trending Recipe"} link={"trending-recipes"}>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12">
+
+          {/* mapping trending recipe */}
+          {
+            TrendingRecipes?.map((item, index)=>(
+              <div key={index}>
+                <MenuItemCard  data={item} />
+              </div>
+            ))
+          }
+        </div>
+      </Container>
+      
+    </>
+  );
+};
+
+export default Home;
