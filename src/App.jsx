aa0f41cd572b8081354345eeec0704654a092f8c
iamxettry <rajuchhetry11@gmail.com>
2024-09-02 09:13:52 +0545

    fix : wrapper component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './App.css'
-import Home from './pages/Home/Home';
-import AboutUs from './pages/About/AboutUs';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import AboutUs from "./pages/About/AboutUs";
+import Navbar from "./Components/layout/Navbar";
+import WrapperContainer from "./Components/common/WrapperContaner";
+import Footer from "./Components/layout/Footer";
 function App() {
   return (
-    <>
-     <Router>
-      <Routes>
-         <Route exact path='/' element={<Home/>} />
-         <Route path='about' element={<AboutUs/>} />
-      </Routes>
-
-     </Router>
-
-    </>
-  )
+    <div className="relative overflow-hidden  scroll-smooth">
+      <Router>
+        <WrapperContainer>
+          <Navbar />
+        </WrapperContainer>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+        </Routes>
+        <Footer/>
+      </Router>
+     
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/pages/About/AboutUs.jsx
+++ src/pages/About/AboutUs.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import Layout from '../../Components/layout/Layout'
 
 const AboutUs = () => {
   return (
-    <Layout>AboutUs</Layout>
+    <>AboutUs</>
   )
 }
 
