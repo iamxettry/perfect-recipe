8dc6bce58477a22e06822c6855b663acdee77c53
iamxettry <rajuchhetry11@gmail.com>
2024-09-02 21:35:17 +0545

    fix : image path

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,7 +2,7 @@ import EggNoodle from "./menuImages/egg-noodle.png"
 import Chris from "./avatar/Chris.jpg"
 import alex from "./avatar/alex.jpg"
 import ShareRecipe from "./menuImages/ShareRecipe.jpg"
-import Ceviche from "./menuImages/Ceviche.jpg"
+import Ceviche from "./menuImages/ceviche.png"
 export  {
     EggNoodle, Chris, ShareRecipe,alex, Ceviche
 }
